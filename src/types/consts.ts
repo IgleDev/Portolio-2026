89aeaf027dcca74298433f6c0cf83aac163c2c82
Adrián Iglesias Fernández <adriiglesias2016@gmail.com>
2026-05-11 21:23:10 +0200

    URL Dinámica

--- src/types/consts.ts
+++ src/types/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'IgleDev | Desarrollo de Software, IA y Automatización';
 export const SITE_DESCRIPTION = 'Desarrollador de Vigo especializado en software a medida, automatización e Inteligencia Artificial para empresas.';
+export const SITE_IMAGE = '/img/imagenPresentacion.webp'
